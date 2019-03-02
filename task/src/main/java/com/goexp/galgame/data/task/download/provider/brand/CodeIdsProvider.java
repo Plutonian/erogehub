03d52b1b0ab70db30615c17b6ab044bb52b866ff
K <benbear0519@gmail.com>
2019-03-03 05:34:40 +0900

    remove mysql support

--- task/src/main/java/com/goexp/galgame/data/task/download/provider/brand/CodeIdsProvider.java
+++ task/src/main/java/com/goexp/galgame/data/task/download/provider/brand/CodeIdsProvider.java
@@ -1,14 +1,10 @@
 package com.goexp.galgame.data.task.download.provider.brand;
 
-import com.goexp.galgame.data.db.query.mysql.BrandService;
 import com.goexp.galgame.data.task.download.provider.IdsProvider;
 
 import java.util.List;
 
 public class CodeIdsProvider implements IdsProvider {
-
-    final private BrandService brandService = new BrandService();
-
 
     @Override
     public List<Integer> getIds() {
--- task/src/main/java/com/goexp/galgame/data/task/download/provider/game/DateRangeIds.java
+++ task/src/main/java/com/goexp/galgame/data/task/download/provider/game/DateRangeIds.java
@@ -1,21 +1,24 @@
 package com.goexp.galgame.data.task.download.provider.game;
 
-import com.goexp.galgame.data.db.query.mysql.GameService;
+import com.goexp.galgame.data.db.query.mongdb.GameQuery;
 import com.goexp.galgame.data.task.download.provider.IdsProvider;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DateRangeIds implements IdsProvider {
 
-    final private GameService gameService = new GameService();
+    final private GameQuery gameService = new GameQuery();
 
     @Override
     public List<Integer> getIds() {
 
         return gameService
-                .idsFrom(LocalDate.of(2019, 1, 1),
+                .list(LocalDate.of(2019, 1, 1),
                         LocalDate.of(2019, 12, 31)
-                );
+                ).stream()
+                .map(game -> game.id)
+                .collect(Collectors.toList());
     }
 }

81dced86569ce98b35c41e6370802bc947542854
K <benbear0519@gmail.com>
2019-04-06 11:12:50 +0900

    improve game name split logic

--- galgamecommon/src/main/java/com/goexp/galgame/common/util/GameName.java
+++ galgamecommon/src/main/java/com/goexp/galgame/common/util/GameName.java
@@ -3,5 +3,5 @@ package com.goexp.galgame.common.util;
 import java.util.regex.Pattern;
 
 public class GameName {
-    public static final Pattern NAME_SPLITER_REX = Pattern.compile("[〜\\s＜「＋]");
+    public static final Pattern NAME_SPLITER_REX = Pattern.compile("[〜\\s　＜「＋]");
 }

c37f672a06f9f9503ee3021fc336119f9640e220
K <benbear0519@gmail.com>
2019-04-07 20:05:11 +0900

    re

--- desktopclient/src/main/java/com/goexp/galgame/gui/view/dataview/simplelistview/cell/HeaderController.java
+++ desktopclient/src/main/java/com/goexp/galgame/gui/view/dataview/simplelistview/cell/HeaderController.java
@@ -101,7 +101,7 @@ public class HeaderController {
         loadWithoutImage(game);
 
         if (game.smallImg != null && game.smallImg.startsWith("http")) {
-            imageImg.setImage(Images.GameImage.tiny(game));
+            imageImg.setImage(Images.GameImage.small(game));
 
         } else {
             imageImg.setImage(null);

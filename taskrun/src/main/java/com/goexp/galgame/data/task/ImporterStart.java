97784bc3cab08941811d6a3cb3e8b1a130e96292
K <benbear0519@gmail.com>
2019-04-09 13:59:10 +0900

    switch to jump-table

--- taskrun/src/main/java/com/goexp/galgame/data/task/ImporterStart.java
+++ taskrun/src/main/java/com/goexp/galgame/data/task/ImporterStart.java
@@ -7,71 +7,68 @@ import com.goexp.galgame.data.task.others.ImportFromLocalAliveBrandTask;
 import com.goexp.galgame.data.task.others.ImportOnceTagTask;
 import com.goexp.galgame.data.task.others.UpdateBrandTask;
 
-import java.util.Map;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
+
+import static java.lang.System.out;
 
 public class ImporterStart {
 
+    private static class Functions {
 
-    private static void howToUse() {
-        var map = Map.of("get-truecv", "Get real CV name",
-                "upgrade-game-date", "Upgrade game info from date to another",
-                "upgrade-game-brand", "Upgrade game info brand by brand",
-                "upgrade-brand", "Upgrade brand info ",
-                "import-tag", "Import tag info once",
-                "import-game-local", "Import from local cache once",
-                "group-brand", "Group brand",
-                "mark-same","Mark the same game"
-        );
+        private final List<Peer> funcTable;
+
+        private Functions() {
+            funcTable = List.of(
+                    new Peer("upgrade-game-date", "Upgrade game info from date to another", FromDateRangeTask::main),
+                    new Peer("upgrade-game-brand", "Upgrade game info brand by brand", FromAliveBrandTask::main),
+                    new Peer("upgrade-brand", "Upgrade brand info ", UpdateBrandTask::main),
+                    new Peer("get-truecv", "Get real CV name", GetTrueCVTask::main),
+                    new Peer("group-brand", "Group brand", GroupBrandTask::main),
+                    new Peer("mark-same", "Mark the same game", MarkSameGameTask::main),
+                    new Peer("import-tag", "Import tag info once", ImportOnceTagTask::main),
+                    new Peer("import-game-local", "Import from local cache once", ImportFromLocalAliveBrandTask::main)
+            );
+        }
+
+        private void showHowToUse() {
+            funcTable.forEach(peer -> out.printf("%s:%s\n", peer.name, peer.desc));
+        }
+
+        private void exec(String[] args) {
+            Objects.requireNonNull(args);
+
+            funcTable.stream()
+                    .filter(peer -> args[0].equals(peer.name))
+                    .findFirst()
+                    .ifPresentOrElse(peer -> peer.run.accept(args)
+                            , this::showHowToUse);
+        }
 
 
-        map.forEach((key, value) -> System.out.printf("%s:%s\n", key, value));
+        private static class Peer {
+            private String name;
+            private String desc;
+            private Consumer<String[]> run;
 
-
+            private Peer(String name, String desc, Consumer<String[]> run) {
+                this.name = name;
+                this.desc = desc;
+                this.run = run;
+            }
+        }
     }
 
-    public static void main(String[] args) {
-        if (args.length == 0) {
-            howToUse();
-        } else {
-            switch (args[0]) {
-                case "get-truecv": {
-                    GetTrueCVTask.main(args);
-                    break;
-                }
-                case "mark-same": {
-                    MarkSameGameTask.main(args);
-                    break;
-                }
-                case "upgrade-game-date": {
-                    FromDateRangeTask.main(args);
-                    break;
-                }
-                case "upgrade-game-brand": {
-                    FromAliveBrandTask.main(args);
-                    break;
-                }
-                case "upgrade-brand": {
-                    UpdateBrandTask.main(args);
-                    break;
-                }
-                case "import-tag": {
-                    ImportOnceTagTask.main(args);
-                    break;
-                }
-                case "import-game-local": {
-                    ImportFromLocalAliveBrandTask.main(args);
-                    break;
-                }
-                case "group-brand": {
-                    GroupBrandTask.main(args);
-                    break;
-                }
-                default: {
-                    howToUse();
-                    break;
-                }
 
-            }
+    public static void main(String[] args) {
+
+        final var func = new Functions();
+
+        if (args.length == 0) {
+            func.showHowToUse();
+        } else {
+            func.exec(args);
         }
 
     }

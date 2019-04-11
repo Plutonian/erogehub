29b3834265936bd47aafbf68bbffbfb8cb2e4fcd
K <benbear0519@gmail.com>
2019-04-11 20:01:51 +0900

    remove unuse code

--- taskrun/src/main/java/com/goexp/galgame/data/task/FromDateRangeTask.scala
+++ taskrun/src/main/java/com/goexp/galgame/data/task/FromDateRangeTask.scala
@@ -1,18 +1,22 @@
 package com.goexp.galgame.data.task
 
+import java.time.LocalDate
+
 import com.goexp.galgame.common.util.Network
 import com.goexp.galgame.data.piplline.core.Piplline
 import com.goexp.galgame.data.task.handler.game.{Bytes2Html, Html2GameOK, LocalGameHandler, ProcessGameOK}
-import com.goexp.galgame.data.task.handler.starter.FromAllBrand
-import com.goexp.galgame.data.task.handler.{DownloadBrandHandler, DownloadGameHandler, MesType, ProcessGameList}
+import com.goexp.galgame.data.task.handler.starter.FromDateRange
+import com.goexp.galgame.data.task.handler.{DownloadGameHandler, MesType, PreProcessGame}
 
-object FromAliveBrandTask {
+object FromDateRangeTask {
   def main(args: Array[String]): Unit = {
     Network.initProxy
 
-    new Piplline(new FromAllBrand)
-      .registryIOTypeMessageHandler(99, new DownloadBrandHandler)
-      .registryCPUTypeMessageHandler(MesType.Brand, new ProcessGameList)
+    val start = LocalDate.now.minusMonths(1).withDayOfMonth(1)
+    val end = LocalDate.now.withMonth(12).withDayOfMonth(31)
+
+    new Piplline(new FromDateRange(start, end))
+      .registryCPUTypeMessageHandler(MesType.PRE_GAME, new PreProcessGame)
       .registryIOTypeMessageHandler(MesType.NEED_DOWN_GAME, new DownloadGameHandler)
       .registryCPUTypeMessageHandler(MesType.Game, new LocalGameHandler)
       .registryCPUTypeMessageHandler(MesType.ContentBytes, new Bytes2Html)

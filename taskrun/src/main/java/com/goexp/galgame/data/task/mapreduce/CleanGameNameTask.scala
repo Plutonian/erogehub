f25f38189eb9c325846c3bc5bc54cc045414c12a
K <benbear0519@gmail.com>
2019-04-11 20:07:42 +0900

    java => scala

---
+++ taskrun/src/main/java/com/goexp/galgame/data/task/mapreduce/CleanGameNameTask.scala
@@ -0,0 +1,17 @@
+package com.goexp.galgame.data.task.mapreduce
+
+import org.atilika.kuromoji.Tokenizer
+
+import scala.collection.JavaConverters._
+
+object CleanGameNameTask {
+  def main(args: Array[String]): Unit = {
+    val tokenizer = Tokenizer.builder.build
+
+    tokenizer.tokenize("続月花美人 〜桜花の頃〜").asScala.toStream
+      .filter(token => token.getBaseForm != null)
+      .foreach(token => {
+        println(s"${token.getSurfaceForm}  {}  ${token.getPartOfSpeech} {}  ${token.isKnown}")
+      })
+  }
+}

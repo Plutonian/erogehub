03dcd4fa3082d48ae365232ef0ef7ecfd2b6d396
K <benbear0519@gmail.com>
2019-04-15 05:42:50 +0900

    mix java&scala

--- taskrun/src/main/java/com/goexp/galgame/data/task/local/CleanGameNameTask.scala
+++ taskrun/src/main/java/com/goexp/galgame/data/task/local/CleanGameNameTask.scala
@@ -1,17 +1,17 @@
 package com.goexp.galgame.data.task.local
 
-import org.atilika.kuromoji.Tokenizer
+//import org.atilika.kuromoji.Tokenizer
 
-import scala.collection.JavaConverters._
+//import scala.collection.JavaConverters._
 
 object CleanGameNameTask {
-  def main(args: Array[String]) = {
-    val tokenizer = Tokenizer.builder.build
-
-    tokenizer.tokenize("続月花美人 〜桜花の頃〜").asScala.toStream
-      .filter(token => token.getBaseForm != null)
-      .foreach(token => {
-        println(s"${token.getSurfaceForm}  {}  ${token.getPartOfSpeech} {}  ${token.isKnown}")
-      })
-  }
+  //  def main(args: Array[String]) = {
+  //    val tokenizer = Tokenizer.builder.build
+  //
+  //    tokenizer.tokenize("続月花美人 〜桜花の頃〜").asScala.toStream
+  //      .filter(token => token.getBaseForm != null)
+  //      .foreach(token => {
+  //        println(s"${token.getSurfaceForm}  {}  ${token.getPartOfSpeech} {}  ${token.isKnown}")
+  //      })
+  //  }
 }

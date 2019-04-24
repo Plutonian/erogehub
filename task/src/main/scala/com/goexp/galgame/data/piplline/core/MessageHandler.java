e08cf0213b8e204c84a17e190d4b2eb48e935b2d
K <benbear0519@gmail.com>
2019-04-24 12:43:15 +0900

    re

--- task/src/main/scala/com/goexp/galgame/data/piplline/core/MessageHandler.java
+++ task/src/main/scala/com/goexp/galgame/data/piplline/core/MessageHandler.java
@@ -2,6 +2,6 @@ package com.goexp.galgame.data.piplline.core;
 
 public interface MessageHandler<T> {
 
-    void process(final Message<T> message, MessageQueueProxy<Message> msgQueue);
+    void process(Message<T> message, MessageQueueProxy<Message> msgQueue);
 
 }

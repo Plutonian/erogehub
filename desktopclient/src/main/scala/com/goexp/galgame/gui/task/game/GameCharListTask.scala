1a72f3439337e920d29ddfe44558e6bdb69c2c30
K <benbear0519@gmail.com>
2019-05-09 02:46:51 +0900

    sort charlist by=>
    1st.キャラ（ＣＶあり、立ち絵あり）
    2nd.キャラ（ＣＶあり、立ち絵なし）
    3ed.キャラ（ＣＶなし、立ち絵なし）

--- desktopclient/src/main/scala/com/goexp/galgame/gui/task/game/GameCharListTask.scala
+++ desktopclient/src/main/scala/com/goexp/galgame/gui/task/game/GameCharListTask.scala
@@ -1,20 +1,31 @@
 package com.goexp.galgame.gui.task.game
 
+import com.goexp.common.util.Strings
 import com.goexp.galgame.common.model.CommonGame.GameCharacter
 import com.goexp.galgame.gui.db.mongo.Query.GameQuery
 import com.mongodb.client.model.Filters
 import javafx.collections.{FXCollections, ObservableList}
 import javafx.concurrent.Task
 
+import scala.collection.JavaConverters._
+
 class GameCharListTask(private[this] val gameId: Int) extends Task[ObservableList[GameCharacter]] {
   override protected def call: ObservableList[GameCharacter] = {
     val g = GameQuery.personTlp.query.where(Filters.eq(gameId)).one
 
-
     Option(g.gameCharacters)
-      .map {
-        FXCollections.observableArrayList(_)
-      }
+      .map(persons => {
+        persons.asScala.toStream
+          .groupBy(p => {
+            if (Strings.isEmpty(p.cv)) {
+              if (Strings.isEmpty(p.img)) 3 else 2
+            } else 1
+          }).toStream
+          .sortBy({ case (k, _) => k })
+          .flatMap({ case (_, v) => v }).asJava
+      })
+      .map(FXCollections.observableArrayList(_))
       .getOrElse(FXCollections.emptyObservableList[GameCharacter])
+
   }
 }

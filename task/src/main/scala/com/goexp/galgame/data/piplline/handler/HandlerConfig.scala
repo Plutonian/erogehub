c22be5c1c411f760a50fa1b6817e27c3cd5ed8e3
K <benbear0519@gmail.com>
2019-05-24 15:35:26 +0900

    add threadCount support

--- task/src/main/scala/com/goexp/galgame/data/piplline/handler/HandlerConfig.scala
+++ task/src/main/scala/com/goexp/galgame/data/piplline/handler/HandlerConfig.scala
@@ -1,8 +1,12 @@
 package com.goexp.galgame.data.piplline.handler
 
-import java.util.concurrent.ExecutorService
+import java.util.concurrent.{ExecutorService, Executors}
 
 import com.goexp.galgame.data.piplline.core.MessageHandler
 
 case class HandlerConfig[T](mesCode: Int, handler: MessageHandler[T], executor: ExecutorService) {
+
+  def this(mesCode: Int, handler: MessageHandler[T], threadCount: Int) =
+    this(mesCode, handler, Executors.newFixedThreadPool(threadCount))
+
 }

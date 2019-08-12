f0765d24dd45cae9341e47f53aa28681d4a41974
benbear <benbear0519@gmail.com>
2019-08-12 12:33:24 +0900

    re

--- task/src/main/scala/com/goexp/galgame/data/task/handler/DownloadGameHandler.scala
+++ task/src/main/scala/com/goexp/galgame/data/task/handler/DownloadGameHandler.scala
@@ -1,6 +1,8 @@
 package com.goexp.galgame.data.task.handler
 
-import com.goexp.galgame.data.piplline.core.{Message, MessageQueueProxy}
+import java.io.IOException
+
+import com.goexp.galgame.data.piplline.core.Message
 import com.goexp.galgame.data.piplline.handler.DefaultMessageHandler
 import com.goexp.galgame.data.task.client.GetChu
 import org.slf4j.LoggerFactory
@@ -11,11 +13,18 @@ class DownloadGameHandler extends DefaultMessageHandler[Int] {
   override def process(message: Message[Int]) = {
     val gid = message.entity
     logger.debug("Download {}", gid)
-    try GetChu.GameService.download(gid)
+    try {
+      GetChu.GameService.download(gid)
+      logger.debug("Download OK:{}",gid)
+    }
     catch {
-      case e: Exception =>
-        logger.error("Re-down:{}", gid)
+      case e: IOException =>
+        logger.error("Re-down:{} IOException:{}", gid,e.getMessage)
         send(new Message[Int](MesType.NEED_DOWN_GAME, gid))
+      case e: Exception =>
+        logger.error("Re-down:{} Exception:{}", gid,e.getMessage)
+        send(new Message[Int](MesType.NEED_DOWN_GAME, gid))
+
     }
     send(new Message[Int](MesType.Game, gid))
   }

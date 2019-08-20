93a34c1ce2585c3447535911afd2045fdfba00e4
benbear <benbear0519@gmail.com>
2019-08-20 18:07:13 +0900

    re

---
+++ gui_view/src/main/java/com/goexp/galgame/gui/view/game/detailview/inner/PersonCellController.java
@@ -0,0 +1,86 @@
+package com.goexp.galgame.gui.view.game.detailview.inner;
+
+import com.goexp.galgame.gui.model.Game;
+import com.goexp.galgame.gui.util.res.Images;
+import com.goexp.galgame.gui.view.DefaultController;
+import com.goexp.galgame.gui.view.game.HomeController;
+import com.goexp.galgame.gui.view.game.detailview.part.CVSearchController;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Region;
+import javafx.scene.text.Text;
+
+public class PersonCellController extends DefaultController {
+    Game.GameCharacter gameChar;
+
+    public Game game;
+
+    @FXML
+    private CVSearchController cvsearchController;
+
+    @FXML
+    private ImageView imageImg;
+
+    @FXML
+    private Text txtName;
+
+    @FXML
+    private MenuButton lbCV;
+
+    @FXML
+    private MenuItem truecv;
+
+    @FXML
+    private Text txtIntro;
+
+    @FXML
+    private Region cvPart;
+
+
+    public void init() {
+
+
+        logger.debug("{}", gameChar);
+
+        txtName.setText(gameChar.name);
+
+
+        boolean isTrueCV = gameChar.trueCV != null && gameChar.trueCV.length() > 0;
+
+        var cv = isTrueCV ? "*" + gameChar.trueCV + "*" : gameChar.cv;
+
+        if (cv != null && cv.length() > 0) {
+            cvPart.setVisible(true);
+            cvsearchController.load(cv);
+            lbCV.setText(cv);
+
+            truecv.setOnAction(event -> {
+                HomeController.$this.loadCVTab(isTrueCV ? gameChar.trueCV : gameChar.cv, isTrueCV);
+            });
+        } else {
+            cvPart.setVisible(false);
+        }
+
+
+        if (gameChar.intro == null || gameChar.intro.trim().length() == 0) {
+            txtIntro.setVisible(false);
+        } else
+            txtIntro.setText(gameChar.intro);
+
+
+        if (gameChar.img != null && gameChar.img.length() > 0) {
+
+            imageImg.setImage(Images.GameImage.GameChar.small(game, gameChar.index, gameChar.img));
+        } else {
+            imageImg.setImage(null);
+        }
+
+    }
+
+    @Override
+    protected void initialize() {
+
+    }
+}
---
+++ gui_view/src/main/java/com/goexp/galgame/gui/view/game/detailview/inner/SimpleImgPartController.java
@@ -0,0 +1,102 @@
+package com.goexp.galgame.gui.view.game.detailview.inner;
+
+import com.goexp.galgame.common.model.CommonGame;
+import com.goexp.galgame.gui.model.Game;
+import com.goexp.galgame.gui.util.res.Images;
+import com.goexp.galgame.gui.view.DefaultController;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+
+public class SimpleImgPartController extends DefaultController {
+
+    private Game game;
+
+    @FXML
+    private ListView<Game.GameImg> listSmallSimple;
+
+    @FXML
+    private ImageView largeSimple;
+
+
+//        private Service<ObservableList<Game.GameImg>> imgListService = new TaskService<>(() -> new GameImgListTask(game.id));
+
+
+    protected void initialize() {
+
+
+//            imgListService.valueProperty().addListener((observable, oldValue, newValue) -> {
+//                if (newValue != null) {
+//                    listSmallSimple.setItems(FXCollections.observableArrayList(newValue));
+//
+//                    if (newValue.size() > 0) {
+//                        listSmallSimple.getSelectionModel().select(0);
+//                    }
+//                }
+//            });
+
+        listSmallSimple.setCellFactory(gameImgListView -> {
+            return new ListCell<>() {
+                @Override
+                protected void updateItem(Game.GameImg item, boolean empty) {
+                    super.updateItem(item, empty);
+
+                    setGraphic(null);
+                    setText(null);
+
+                    if (!empty) {
+                        setGraphic(new ImageView(Images.GameImage.Simple.small(game, item.index, item.src)));
+                    }
+
+                }
+            };
+        });
+
+        listSmallSimple.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, simpleLargeImage) -> {
+
+            if (simpleLargeImage != null) {
+                largeSimple.setImage(Images.GameImage.Simple.large(game, simpleLargeImage.index, simpleLargeImage.src));
+            }
+        });
+
+    }
+
+
+    public void load(Game game) {
+
+        this.game = game;
+        listSmallSimple.getSelectionModel().clearSelection();
+        largeSimple.setImage(null);
+
+        listSmallSimple.setItems(FXCollections.observableArrayList(game.gameImgs));
+        listSmallSimple.getSelectionModel().select(0);
+
+        logger.debug("{}", game);
+
+    }
+
+
+    @FXML
+    private void btnPrev_OnAction(ActionEvent event) {
+        var index = listSmallSimple.getSelectionModel().getSelectedIndex();
+        index--;
+        if (index >= 0) {
+            listSmallSimple.getSelectionModel().select(index);
+            listSmallSimple.scrollTo(index);
+        }
+    }
+
+    @FXML
+    private void btnNext_OnAction(ActionEvent event) {
+        var index = listSmallSimple.getSelectionModel().getSelectedIndex();
+        index++;
+        if (index < listSmallSimple.getItems().size()) {
+            listSmallSimple.getSelectionModel().select(index);
+            listSmallSimple.scrollTo(index);
+        }
+    }
+
+}

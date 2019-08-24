8a0a6ec13b732106e8d22a7c9c9acbf1f87414d9
benbear <benbear0519@gmail.com>
2019-08-25 00:26:47 +0900

    re

---
+++ gui_view/src/main/scala/com/goexp/galgame/gui/HGameApp.scala
@@ -0,0 +1,36 @@
+package com.goexp.galgame.gui
+
+import com.goexp.galgame.common.util.Network
+import com.goexp.galgame.gui.util.FXMLLoaderProxy
+import com.goexp.galgame.gui.view.MainController
+import javafx.application.Application
+import javafx.scene.paint.Color
+import javafx.scene.{Parent, Scene}
+import javafx.stage.Stage
+
+object HGameApp extends App {
+  var app: HGameApp = _
+
+  //  def main(args: Array[String]): Unit =
+  Application.launch(classOf[HGameApp])
+
+
+}
+
+class HGameApp extends Application {
+  override def init(): Unit = {
+    HGameApp.app = this
+    Network.initProxy()
+  }
+
+  override def start(primaryStage: Stage): Unit = {
+    val proxy = new FXMLLoaderProxy[Parent, MainController]("HGame.fxml")
+    primaryStage.setTitle("エロゲ まとめ")
+    primaryStage.setWidth(1400)
+    primaryStage.setMinWidth(1200)
+    primaryStage.setHeight(800)
+    primaryStage.setMinHeight(800)
+    primaryStage.setScene(new Scene(proxy.node, Color.BLACK))
+    primaryStage.show()
+  }
+}

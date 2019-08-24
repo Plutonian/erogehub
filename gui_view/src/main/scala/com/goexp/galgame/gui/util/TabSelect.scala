2ae4707da93c89bb82f2dc6c08fb638e21291314
benbear <benbear0519@gmail.com>
2019-08-25 00:41:25 +0900

    re

---
+++ gui_view/src/main/scala/com/goexp/galgame/gui/util/TabSelect.scala
@@ -0,0 +1,31 @@
+package com.goexp.galgame.gui.util
+
+import com.goexp.galgame.gui.view.game.HomeController
+import javafx.scene.control.{Tab, TabPane}
+
+import scala.jdk.CollectionConverters._
+
+object TabSelect {
+  def from(root: TabPane) = new TabSelect(root)
+
+  def from = new TabSelect(HomeController.$this.mainTabPanel)
+}
+
+class TabSelect private(val root: TabPane) {
+  private var notFind: () => Tab = _
+
+  def ifNotFind(notFind: () => Tab): TabSelect = {
+    this.notFind = notFind
+    this
+  }
+
+  def select(title: String): Unit =
+    root.getTabs.asScala.to(LazyList)
+      .find(_.getText == title) match {
+      case Some(tab) =>
+        root.getSelectionModel.select(tab)
+      case None =>
+        HomeController.$this.insertTab(notFind())
+    }
+
+}

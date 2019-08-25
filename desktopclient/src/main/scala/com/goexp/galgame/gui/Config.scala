3c316b6d9f8274a1f1503ee74f97059a0a98556f
benbear <benbear0519@gmail.com>
2019-08-25 18:55:22 +0900

    re

--- desktopclient/src/main/scala/com/goexp/galgame/gui/Config.scala
+++ desktopclient/src/main/scala/com/goexp/galgame/gui/Config.scala
@@ -1,8 +1,7 @@
 package com.goexp.galgame.gui
 
-import java.nio.file.Path
 import com.goexp.galgame.common.Config.DATA_ROOT
 
 object Config {
-  val IMG_PATH: Path = DATA_ROOT.resolve("img/game/")
+  val IMG_PATH = DATA_ROOT.resolve("img/game/")
 }

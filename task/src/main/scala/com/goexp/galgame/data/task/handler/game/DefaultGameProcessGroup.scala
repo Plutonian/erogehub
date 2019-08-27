b88751a3888224a38fbc05b1264d8b7ea3719c51
benbear <benbear0519@gmail.com>
2019-08-27 23:10:07 +0900

    re

--- task/src/main/scala/com/goexp/galgame/data/task/handler/game/DefaultGameProcessGroup.scala
+++ task/src/main/scala/com/goexp/galgame/data/task/handler/game/DefaultGameProcessGroup.scala
@@ -4,7 +4,7 @@ import com.goexp.galgame.data.piplline.handler.{HandlerConfig, HandlerConfigGrou
 import com.goexp.galgame.data.task.handler.MesType
 
 object DefaultGameProcessGroup extends HandlerConfigGroup(
-  new HandlerConfig(MesType.Game, new LocalGameHandler, 2),
+  new HandlerConfig(MesType.Game, new LocalGameHandler, Runtime.getRuntime.availableProcessors()),
   new HandlerConfig(MesType.ContentBytes, new Bytes2Html, 30),
   new HandlerConfig(MesType.ContentHtml, new Html2GameOK, 30),
   new HandlerConfig(MesType.GAME_OK, new ProcessGameOK, 30)) {

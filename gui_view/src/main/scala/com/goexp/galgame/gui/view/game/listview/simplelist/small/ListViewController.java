78254ea1e240fe0b1a9c9f37956aa042259f0cae
benbear <benbear0519@gmail.com>
2019-08-28 19:07:20 +0900

    re

--- gui_view/src/main/scala/com/goexp/galgame/gui/view/game/listview/simplelist/small/ListViewController.java
+++ gui_view/src/main/scala/com/goexp/galgame/gui/view/game/listview/simplelist/small/ListViewController.java
@@ -2,14 +2,19 @@ package com.goexp.galgame.gui.view.game.listview.simplelist.small;
 
 import com.goexp.galgame.gui.model.Game;
 import com.goexp.galgame.gui.util.FXMLLoaderProxy;
+import com.goexp.galgame.gui.util.cache.Cache;
+import com.goexp.galgame.gui.util.cache.CellCache;
 import com.goexp.galgame.gui.view.DefaultController;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
 
 
 public class ListViewController extends DefaultController {
+
+//    static Cache cache=new CellCache<Integer, Node>();
 
     @FXML
     private ListView<Game> smallListSimple;
@@ -26,6 +31,7 @@ public class ListViewController extends DefaultController {
                 protected void updateItem(Game item, boolean empty) {
                     super.updateItem(item, empty);
                     setGraphic(null);
+                    setText(null);
 
                     if (item != null && !empty) {
                         loader.controller.load(item);

7c5fd0c165201c1ac3b065606dffbe9d5fcf91f5
benbear <benbear0519@gmail.com>
2019-08-29 01:51:43 +0900

    fixbug

--- desktopclient/src/main/scala/com/goexp/galgame/gui/util/cache/Cache.scala
+++ desktopclient/src/main/scala/com/goexp/galgame/gui/util/cache/Cache.scala
@@ -1,5 +1,6 @@
 package com.goexp.galgame.gui.util.cache
 
+
 import java.lang.ref.SoftReference
 
 import scala.collection.mutable
@@ -8,7 +9,13 @@ import scala.collection.mutable
 class Cache[K, V]() {
   private[this] val imageCache = mutable.Map[K, SoftReference[V]]()
 
-  def get(key: K) = imageCache.get(key).map(_.get)
+  def get(key: K) = {
+    if (imageCache.contains(key)) {
+      Option(imageCache(key).get())
+    } else {
+      None
+    }
+  }
 
   def put(key: K, value: V) =
     imageCache.put(key, new SoftReference[V](value))

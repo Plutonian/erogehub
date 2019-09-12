a8003c69629cf5b0f1c591f16fc8a61d644b16c6
benbear <benbear0519@gmail.com>
2019-09-12 19:16:00 +0900

    re

--- taskrun/src/main/scala/com/goexp/galgame/data/task/others/UpdateBrandTask.scala
+++ taskrun/src/main/scala/com/goexp/galgame/data/task/others/UpdateBrandTask.scala
@@ -4,42 +4,54 @@ import com.goexp.common.util.string.Strings
 import com.goexp.galgame.common.util.Network
 import com.goexp.galgame.data.db.importor.mongdb.BrandDB
 import com.goexp.galgame.data.db.query.mongdb.BrandQuery
+import com.goexp.galgame.data.task.ansyn.Pool._
 import com.goexp.galgame.data.task.client.GetChu.BrandService
 import org.slf4j.LoggerFactory
 
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
 import scala.jdk.CollectionConverters._
 
-object UpdateBrandTask {
+object UpdateBrandTask extends App {
   private val logger = LoggerFactory.getLogger(UpdateBrandTask.getClass)
 
-  def main(args: Array[String]) = {
 
-    Network.initProxy()
+  Network.initProxy()
 
-    val localMap = BrandQuery.tlp.query
-      .list.asScala.to(LazyList)
-      .map(b => b.id -> b)
-      .toMap
+  val localMap = BrandQuery.tlp.query
+    .list.asScala.to(LazyList)
+    .map(b => b.id -> b)
+    .toMap
 
-    logger.info(s"Local:${localMap.size}")
+  logger.info(s"Local:${localMap.size}")
 
 
-    val remotes = BrandService.all().toSet
-    logger.info(s"Remote: ${remotes.size}")
+  val remotes = BrandService.all().toSet
+  logger.info(s"Remote: ${remotes.size}")
 
-    remotes.foreach(remote => {
-      localMap.get(remote.id) match {
-        // find in local
-        case Some(local) =>
-          if (Strings.isEmpty(local.website) && Strings.isNotEmpty(remote.website)) {
-            logger.info(s"Local: $local,Remote: $remote")
+  remotes.foreach(remote => {
+    localMap.get(remote.id) match {
+      // find in local
+      case Some(local) =>
+        if (Strings.isEmpty(local.website) && Strings.isNotEmpty(remote.website)) {
+          logger.info(s"Local: $local,Remote: $remote")
+          val f = Future {
             BrandDB.updateWebsite(remote)
-          }
-        case None =>
-          logger.info(s"<Insert> $remote")
-          BrandDB.insert(remote)
-      }
+          }(IO_POOL)
 
-    })
-  }
+          Await.result(f, 10.minutes)
+
+        }
+      case None =>
+        logger.info(s"<Insert> $remote")
+        val f = Future {
+          BrandDB.insert(remote)
+
+        }(IO_POOL)
+
+        Await.result(f, 10.minutes)
+
+    }
+
+  })
 }

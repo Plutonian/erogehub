468e3dbe0a021fc4bbd7069683325d90239a1b16
benbear <benbear0519@gmail.com>
2019-10-22 19:53:29 +0900

    java=>scala

--- desktopclient/src/main/scala/com/goexp/galgame/gui/task/TagListTask.scala
+++ desktopclient/src/main/scala/com/goexp/galgame/gui/task/TagListTask.scala
@@ -3,10 +3,11 @@ package com.goexp.galgame.gui.task
 import com.goexp.galgame.common.model.TagType
 import com.goexp.galgame.gui.db.mongo.query.TagQuery
 import com.mongodb.client.model.Sorts.ascending
-import javafx.collections.{FXCollections, ObservableList}
 import javafx.concurrent.Task
 
-class TagListTask extends Task[ObservableList[TagType]] {
-  override protected def call: ObservableList[TagType] =
-    FXCollections.observableArrayList(TagQuery.tlp.query.sort(ascending("order")).list)
+import scala.collection.mutable
+
+class TagListTask extends Task[mutable.Buffer[TagType]] {
+  override protected def call =
+    TagQuery.tlp.query.sort(ascending("order")).scalaList
 }

4c1f91bcd17e5d402fb9d75ec7851a92593e9895
benbear <benbear0519@gmail.com>
2019-10-23 11:59:57 +0900

    java=>scala

--- desktopclient/src/main/scala/com/goexp/galgame/gui/task/game/search/ByTag.scala
+++ desktopclient/src/main/scala/com/goexp/galgame/gui/task/game/search/ByTag.scala
@@ -3,13 +3,17 @@ package com.goexp.galgame.gui.task.game.search
 import com.goexp.galgame.gui.db.mongo.query.GameQuery
 import com.goexp.galgame.gui.model.Game
 import com.mongodb.client.model.Filters
+import com.mongodb.client.model.Filters._
 import javafx.collections.{FXCollections, ObservableList}
 import javafx.concurrent.Task
 
 class ByTag(private[this] val tag: String) extends Task[ObservableList[Game]] {
   override protected def call: ObservableList[Game] = {
     val list = GameQuery.tlp.query
-      .where(Filters.eq("tag", tag))
+      .where(Filters.or(
+        Filters.eq("tag", tag),
+        regex("name", tag)
+      ))
       .list
 
 

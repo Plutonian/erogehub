4f7d530e2365492fb432c0204c137b7c7d7faeaa
benbear <benbear0519@gmail.com>
2019-10-23 10:55:03 +0900

    java=>scala

---
+++ gui_view/src/main/scala/com/goexp/galgame/gui/view/common/jump/JumpBrandController.scala
@@ -0,0 +1,32 @@
+package com.goexp.galgame.gui.view.common.jump
+
+import com.goexp.common.util.string.Strings
+import com.goexp.galgame.common.website.getchu.GameList
+import com.goexp.galgame.gui.model.Brand
+import com.goexp.galgame.gui.util.Websites
+import com.goexp.galgame.gui.view.DefaultController
+import com.goexp.galgame.gui.view.game.HomeController
+import javafx.fxml.FXML
+import javafx.scene.control.{MenuButton, MenuItem}
+
+class JumpBrandController extends DefaultController {
+  private var brand = new Brand
+  @FXML private var searchLinkController: SearchController = _
+  @FXML private var brandLabel: MenuButton = _
+  @FXML private var jumpBrand: MenuItem = _
+  @FXML private var linkWebsite: MenuItem = _
+  @FXML private var linkGetchu: MenuItem = _
+
+  override protected def initialize() = {
+    linkGetchu.setOnAction(_ => Websites.open(GameList.byBrand(brand.id)))
+    jumpBrand.setOnAction(_ => HomeController.$this.viewBrand(brand))
+    linkWebsite.setOnAction(_ => Websites.open(brand.website))
+  }
+
+  def load(brand: Brand) = {
+    this.brand = brand
+    brandLabel.setText(brand.name)
+    linkWebsite.setVisible(Strings.isNotEmpty(brand.website))
+    searchLinkController.load(brand.name)
+  }
+}

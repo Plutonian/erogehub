0f1bdef3f30f561d63d8264232fa4b70af2af2b2
benbear <benbear0519@gmail.com>
2019-10-28 15:09:07 +0900

    remove mysql

--- db_mysql/src/main/java/com/goexp/db/mysql/ObjectCreator.java
+++
@@ -1,8 +0,0 @@
-package com.goexp.db.mysql;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public interface ObjectCreator<T> {
-    T create(ResultSet resultSet) throws SQLException;
-}

55792dd132cb4ea582f0bc3422ec5f7d4c801d17
benbear <benbear0519@gmail.com>
2019-10-28 19:00:32 +0900

    java=>scala

---
+++ gui_view/src/main/scala/com/goexp/galgame/gui/view/game/listview/sidebar/FilterController.scala
@@ -0,0 +1,14 @@
+package com.goexp.galgame.gui.view.game.listview.sidebar
+
+import java.util
+import java.util.function.Predicate
+
+import com.goexp.galgame.gui.view.DefaultController
+import javafx.beans.property.SimpleBooleanProperty
+
+abstract class FilterController[T] extends DefaultController {
+  final val onSetProperty = new SimpleBooleanProperty(false)
+  var predicate: Predicate[T] = _
+
+  def init(filteredGames: util.List[T]): Unit
+}

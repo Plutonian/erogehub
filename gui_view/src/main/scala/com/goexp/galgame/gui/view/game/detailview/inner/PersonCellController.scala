60aae2f8dd36380308f7b780da832193650f18ee
benbear <benbear0519@gmail.com>
2019-10-28 21:49:14 +0900

    java=>scala

---
+++ gui_view/src/main/scala/com/goexp/galgame/gui/view/game/detailview/inner/PersonCellController.scala
@@ -0,0 +1,60 @@
+package com.goexp.galgame.gui.view.game.detailview.inner
+
+import com.goexp.common.util.string.Strings
+import com.goexp.galgame.common.model.game.GameCharacter
+import com.goexp.galgame.gui.model.Game
+import com.goexp.galgame.gui.util.res.gameimg.PersonImage
+import com.goexp.galgame.gui.view.DefaultController
+import com.goexp.galgame.gui.view.game.HomeController
+import com.goexp.galgame.gui.view.game.detailview.part.CVSearchController
+import javafx.fxml.FXML
+import javafx.scene.control.{MenuButton, MenuItem}
+import javafx.scene.image.ImageView
+import javafx.scene.layout.Region
+import javafx.scene.text.Text
+
+class PersonCellController extends DefaultController {
+  var gameChar: GameCharacter = _
+  var game: Game = _
+  @FXML private var cvsearchController: CVSearchController = _
+  @FXML private var imageImg: ImageView = _
+  @FXML private var txtName: Text = _
+  @FXML private var lbCV: MenuButton = _
+  @FXML private var truecv: MenuItem = _
+  @FXML private var txtIntro: Text = _
+  @FXML private var cvPart: Region = _
+
+  def init() = {
+    logger.debug("{}", gameChar)
+    txtName.setText(gameChar.name)
+    val isTrueCV = Strings.isNotEmpty(gameChar.trueCV)
+    val cv = if (isTrueCV) s"*${gameChar.trueCV}*" else gameChar.cv
+
+    if (Strings.isNotEmpty(cv)) {
+      cvPart.setVisible(true)
+      cvsearchController.load(cv)
+      lbCV.setText(cv)
+      truecv.setOnAction(_ => {
+        val sCV = if (isTrueCV) gameChar.trueCV else gameChar.cv
+        HomeController.$this.loadCVTab(sCV, isTrueCV)
+      })
+    }
+    else
+      cvPart.setVisible(false)
+
+    if (Strings.isEmpty(gameChar.intro))
+      txtIntro.setVisible(false)
+    else
+      txtIntro.setText(gameChar.intro)
+
+    if (Strings.isNotEmpty(gameChar.img)) {
+      val image = new PersonImage(game).small(gameChar.index, gameChar.img)
+      imageImg.setImage(image)
+    }
+    else
+      imageImg.setImage(null)
+  }
+
+  override protected def initialize() = {
+  }
+}
---
+++ gui_view/src/main/scala/com/goexp/galgame/gui/view/game/detailview/inner/SimpleImgPartController.scala
@@ -0,0 +1,64 @@
+package com.goexp.galgame.gui.view.game.detailview.inner
+
+import com.goexp.galgame.common.model.game.GameImg
+import com.goexp.galgame.gui.model.Game
+import com.goexp.galgame.gui.util.res.gameimg.SimpleImage
+import com.goexp.galgame.gui.view.DefaultController
+import javafx.collections.FXCollections
+import javafx.event.ActionEvent
+import javafx.fxml.FXML
+import javafx.scene.control.{ListCell, ListView}
+import javafx.scene.image.ImageView
+
+class SimpleImgPartController extends DefaultController {
+  private var game: Game = _
+  @FXML private var listSmallSimple: ListView[GameImg] = _
+  @FXML private var largeSimple: ImageView = _
+
+  override protected def initialize() = {
+    listSmallSimple.setCellFactory(_ => new ListCell[GameImg]() {
+      override protected def updateItem(item: GameImg, empty: Boolean) = {
+        super.updateItem(item, empty)
+        setGraphic(null)
+        setText(null)
+        if (!empty) {
+          val image = new SimpleImage(game).small(item.index, item.src)
+          setGraphic(new ImageView(image))
+        }
+      }
+    })
+    listSmallSimple.getSelectionModel.selectedItemProperty.addListener((_, _, simpleLargeImage) => {
+      if (simpleLargeImage != null) {
+        val img = new SimpleImage(game).large(simpleLargeImage.index, simpleLargeImage.src)
+        largeSimple.setImage(img)
+      }
+    })
+  }
+
+  def load(game: Game) = {
+    this.game = game
+    listSmallSimple.getSelectionModel.clearSelection()
+    largeSimple.setImage(null)
+    listSmallSimple.setItems(FXCollections.observableArrayList(game.gameImgs))
+    listSmallSimple.getSelectionModel.select(0)
+    logger.debug("{}", game)
+  }
+
+  @FXML private def btnPrev_OnAction(event: ActionEvent) = {
+    var index = listSmallSimple.getSelectionModel.getSelectedIndex
+    index -= 1
+    if (index >= 0) {
+      listSmallSimple.getSelectionModel.select(index)
+      listSmallSimple.scrollTo(index)
+    }
+  }
+
+  @FXML private def btnNext_OnAction(event: ActionEvent) = {
+    var index = listSmallSimple.getSelectionModel.getSelectedIndex
+    index += 1
+    if (index < listSmallSimple.getItems.size) {
+      listSmallSimple.getSelectionModel.select(index)
+      listSmallSimple.scrollTo(index)
+    }
+  }
+}

9b493827d434fdf576fa2a36d2d0008233cf4f2a
benbear <benbear0519@gmail.com>
2019-11-02 17:32:51 +0900

    new sendTo method

--- piplline/src/main/scala/com/goexp/piplline/core/MessageDriven.scala
+++ piplline/src/main/scala/com/goexp/piplline/core/MessageDriven.scala
@@ -1,6 +1,7 @@
 package com.goexp.piplline.core
 
 import scala.beans.BeanProperty
+import scala.reflect.ClassTag
 
 private[piplline]
 trait MessageDriven {
@@ -8,8 +9,10 @@ trait MessageDriven {
   @BeanProperty
   var queue: MessageQueueProxy[Message] = _
 
-  def send(mes: Message) = queue.offer(mes)
+  def send(mes: Message): Unit = queue.offer(mes)
 
-  def sendTo(target: Class[_ <: MessageHandler], entity: Any) = queue.offer(Message(target, entity))
+  def sendTo(target: Class[_ <: MessageHandler], entity: Any): Unit = queue.offer(Message(target, entity))
+
+  def sendTo[TO <: MessageHandler](entity: Any)(implicit ct: ClassTag[TO]): Unit = queue.offer(Message(ct.runtimeClass.asInstanceOf[Class[MessageHandler]], entity))
 
 }

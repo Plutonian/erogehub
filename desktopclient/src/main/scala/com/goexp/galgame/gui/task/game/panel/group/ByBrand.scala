7429f56ef53e6e48f8101c29c48b4f2cc7bb4451
benbear <benbear0519@gmail.com>
2019-12-19 06:08:26 +0900

    add groupBy check

--- desktopclient/src/main/scala/com/goexp/galgame/gui/task/game/panel/group/ByBrand.scala
+++ desktopclient/src/main/scala/com/goexp/galgame/gui/task/game/panel/group/ByBrand.scala
@@ -4,17 +4,28 @@ import java.util
 
 import com.goexp.galgame.gui.model.{Brand, Game}
 import com.goexp.galgame.gui.task.game.panel.group.node._
+import com.typesafe.scalalogging.Logger
 import javafx.concurrent.Task
 import javafx.scene.control.TreeItem
 
 import scala.jdk.CollectionConverters._
 
 class ByBrand(val groupGames: util.List[Game]) extends Task[TreeItem[DefaultItem]] {
+  val logger = Logger(classOf[ByBrand])
+
   override protected def call: TreeItem[DefaultItem] = createCompGroup(groupGames)
 
   private def createCompGroup(filteredGames: util.List[Game]) = {
     val compsNodes = filteredGames.asScala.to(LazyList)
-      .groupBy(game => Option(game.brand.comp).getOrElse(game.brand)).to(LazyList)
+      .groupBy { game =>
+        val brand = game.brand
+
+        if (brand == null)
+          logger.error(s"brand is null,game[${game.id}] ${game.name}")
+        else
+          Option(brand.comp).getOrElse(brand)
+      }
+      .to(LazyList)
       .sortBy({ case (_, v) => v.size }).reverse
       .map({
         case (comp: String, v) =>

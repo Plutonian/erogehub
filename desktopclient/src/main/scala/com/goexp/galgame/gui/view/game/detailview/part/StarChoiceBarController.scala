d90cc8261253ccbc9acb7430e2b2dd1212bc3a1f
benbear <benbear0519@gmail.com>
2020-02-13 04:04:55 +0900

    use rating control

--- desktopclient/src/main/scala/com/goexp/galgame/gui/view/game/detailview/part/StarChoiceBarController.scala
+++ desktopclient/src/main/scala/com/goexp/galgame/gui/view/game/detailview/part/StarChoiceBarController.scala
@@ -1,70 +1,39 @@
 package com.goexp.galgame.gui.view.game.detailview.part
 
-import java.util
-
 import com.goexp.galgame.gui.model.Game
-import com.goexp.ui.javafx.TaskService
 import com.goexp.galgame.gui.task.game.change.Star
-import com.goexp.ui.javafx.DefaultController
-import javafx.beans.property.SimpleBooleanProperty
+import com.goexp.ui.javafx.{DefaultController, TaskService}
 import javafx.beans.value.ChangeListener
 import javafx.fxml.FXML
-import javafx.scene.control.{Toggle, ToggleButton, ToggleGroup}
-import javafx.scene.layout.HBox
-
-import scala.jdk.CollectionConverters._
+import org.controlsfx.control.Rating
 
 class StarChoiceBarController extends DefaultController {
-  val onStarChangeProperty = new SimpleBooleanProperty(false)
+  @FXML
+  var starRating: Rating = _
 
   private var targetGame: Game = _
-  private var handler: ChangeListener[Toggle] = (_, _, newV) => {
-    if (newV == null) {
-      targetGame.star = 0
-      changeStarService.restart()
-    }
-    else {
-      logger.debug(s"New:${newV.getUserData}")
 
-      targetGame.star = newV.getUserData.asInstanceOf[Int]
+  private var starHandler: ChangeListener[Number] = (_, _, newV) => {
+    if (newV != null) {
+      logger.debug(s"New:${newV.intValue()}")
+
+      targetGame.star = newV.intValue()
       changeStarService.restart()
     }
-    onStarChangeProperty.set(true)
-    onStarChangeProperty.set(false)
   }
-  private var list: util.List[ToggleButton] = _
-  @FXML private var groupLikeCon: HBox = _
-  private val groupLike = new ToggleGroup
+
   private val changeStarService = TaskService(new Star(targetGame))
 
   override protected def initialize() = {
-    list = Range.inclusive(1, 5)
-      .map(star => {
-        val toggle = new ToggleButton
-        toggle.setUserData(star)
-        toggle.setText(star.toString)
-        toggle.setToggleGroup(groupLike)
-        toggle
-      }).asJava
-
-    groupLikeCon.getChildren.setAll(list)
-
   }
 
   def load(game: Game) = {
     this.targetGame = game
 
+    starRating.ratingProperty().removeListener(starHandler)
+    starRating.setRating(game.star)
+    starRating.ratingProperty().addListener(starHandler)
 
-    groupLike.selectedToggleProperty.removeListener(handler)
-
-    list.forEach(_.setSelected(false))
-    list.asScala
-      .find(btn => btn.getUserData.asInstanceOf[Int] == game.star)
-      .foreach {
-        _.setSelected(true)
-      }
-
-    groupLike.selectedToggleProperty.addListener(handler)
   }
 
 }

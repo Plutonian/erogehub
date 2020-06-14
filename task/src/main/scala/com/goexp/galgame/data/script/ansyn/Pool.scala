f188a6b77141e26afc45b87a7adfef2166dc3a55
benbear <benbear0519@gmail.com>
2020-06-14 19:55:27 +0800

    re Pool

--- task/src/main/scala/com/goexp/galgame/data/script/ansyn/Pool.scala
+++ task/src/main/scala/com/goexp/galgame/data/script/ansyn/Pool.scala
@@ -6,7 +6,13 @@ import scala.concurrent.ExecutionContext
 
 object Pool {
 
-  val IO_POOL = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(30, new TaskThreadFactory))
-  val CPU_POOL = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(Runtime.getRuntime.availableProcessors(), new TaskThreadFactory))
+  val DOWN_POOL_SERV = Executors.newFixedThreadPool(30, new TaskThreadFactory)
+  val DB_POOL_SERV = Executors.newFixedThreadPool(10, new TaskThreadFactory)
+  val CPU_POOL_SERV = Executors.newFixedThreadPool(Runtime.getRuntime.availableProcessors(), new TaskThreadFactory)
+
+
+  val DB_POOL = ExecutionContext.fromExecutor(DB_POOL_SERV)
+  val DOWN_POOL = ExecutionContext.fromExecutor(DOWN_POOL_SERV)
+  implicit val CPU_POOL = ExecutionContext.fromExecutor(CPU_POOL_SERV)
 
 }

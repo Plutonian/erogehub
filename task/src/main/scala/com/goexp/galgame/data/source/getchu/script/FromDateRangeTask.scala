3a927163f4d7622b6ac920b40a9a2302e2bb1d26
benbear <benbear0519@gmail.com>
2021-01-31 05:14:19 +0800

    re

--- task/src/main/scala/com/goexp/galgame/data/source/getchu/script/FromDateRangeTask.scala
+++ task/src/main/scala/com/goexp/galgame/data/source/getchu/script/FromDateRangeTask.scala
@@ -1,26 +1,39 @@
 package com.goexp.galgame.data.source.getchu.script
 
-import java.time.LocalDate
-
 import com.goexp.galgame.common.util.Network
 import com.goexp.galgame.data.source.getchu.actor.{ActorConfig, DownloadPageActor}
 import com.goexp.piplline.core.Pipeline
+
+import java.time.LocalDate
 
 object FromDateRangeTask {
   def main(args: Array[String]) = {
     Network.initProxy()
 
-    val start = LocalDate.now.minusMonths(1).withDayOfMonth(1)
+    args.length match {
+      case 0 =>
+        val start = LocalDate.now.minusMonths(1).withDayOfMonth(1)
 
-    val end = start.plusMonths(6)
-    //    val end = LocalDate.now.withMonth(12).withDayOfMonth(31)
+        val end = start.plusMonths(6)
 
+        val pipeline = new Pipeline()
+          .regGroup(ActorConfig.DefaultGameProcessGroup)
+          .start()
 
-    val pipeline = new Pipeline()
-      .regGroup(ActorConfig.DefaultGameProcessGroup)
-      .start()
+        pipeline.sendTo[DownloadPageActor]((start, end))
 
-    pipeline.sendTo[DownloadPageActor]((start, end))
+      case 2 =>
+        val start = LocalDate.parse(args(0))
+        val end = LocalDate.parse(args(1))
+
+        val pipeline = new Pipeline()
+          .regGroup(ActorConfig.DefaultGameProcessGroup)
+          .start()
+
+        pipeline.sendTo[DownloadPageActor]((start, end))
+      case _ =>
+        println("<start> <end>")
+    }
 
 
   }

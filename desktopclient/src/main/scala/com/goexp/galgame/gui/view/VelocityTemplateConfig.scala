200b4476ccc76a4d896ae45d919179e4f543507e
benbear <benbear0519@gmail.com>
2022-05-10 02:48:59 +0800

    use Velocity

--- desktopclient/src/main/scala/com/goexp/galgame/gui/view/VelocityTemplateConfig.scala
+++ desktopclient/src/main/scala/com/goexp/galgame/gui/view/VelocityTemplateConfig.scala
@@ -6,7 +6,6 @@ import org.apache.velocity.runtime.RuntimeConstants
 import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader
 
 import java.io.StringWriter
-import scala.util.chaining.scalaUtilChainingOps
 
 object VelocityTemplateConfig {
 
@@ -18,16 +17,7 @@ object VelocityTemplateConfig {
       writer.flush()
       writer.toString // n
     }
-
   }
-
-  import freemarker.template.{Configuration, TemplateExceptionHandler}
-
-  // Create your Configuration instance, and specify if up to what FreeMarker// Create your Configuration instance, and specify if up to what FreeMarker
-
-  // version (here 2.3.22) do you want to apply the fixes that are not 100%
-  // backward-compatible. See the Configuration JavaDoc for details.
-  //  val cfg = new Configuration(Configuration.VERSION_2_3_31)
 
   val ve = new VelocityEngine
 
@@ -40,21 +30,6 @@ object VelocityTemplateConfig {
     ve.setProperty("resource.loader.classpath.class", classOf[ClasspathResourceLoader].getName)
     ve.init
 
-  }
-
-  def ok() = {
-    // 获取模板文件
-    val t = ve.getTemplate("/com/goexp/galgame/gui/view/css.css")
-    // 设置变量
-
-    val ctx = new VelocityContext
-    //    ctx.put("name", "Velocity")
-    //    val list =  List(1,2)
-    //    ctx.put("list", list)
-    // 输出
-    val sw = new StringWriter
-    t.merge(ctx, sw)
-    System.out.println(sw.toString)
   }
 
 

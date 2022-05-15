ca0497eb80f0d600942f62fdde5d07bd41a27468
benbear <benbear0519@gmail.com>
2022-05-15 13:02:09 +0800

    update

--- desktopclient/src/main/scala/com/goexp/galgame/gui/view/game/DateController.scala
+++ desktopclient/src/main/scala/com/goexp/galgame/gui/view/game/DateController.scala
@@ -21,75 +21,68 @@ class DateController extends DefaultController {
   @FXML private var flowMonth: ListView[Int] = _
   @FXML private var flowYear: ListView[Int] = _
 
-  override protected def initialize() = {
 
-    def initMonth() = {
+  override protected def initComponent(): Unit = {
 
-      //cell
-      flowMonth.setCellFactory { _ =>
-        TextListCell[Int] { month =>
-          if (month == LocalDate.now.getMonthValue) "今月" else s"${month}月"
-        }
+    /**
+     * month
+     */
+    flowMonth.setCellFactory { _ =>
+      TextListCell[Int] { month =>
+        if (month == LocalDate.now.getMonthValue) "今月" else s"${month}月"
       }
-
-      //selcet event
-      flowMonth.getSelectionModel().selectedItemProperty().addListener { (_, _, month) =>
-        val from = LocalDate.now.withMonth(month).withDayOfMonth(1)
-        val to = from.plusMonths(1).minusDays(1)
-        val title = s"${month}月"
-
-        logger.debug(s"Range:${from}  ${to}")
-
-        TabManager().open(title, {
-          new DataPage(ExplorerData(new ByDateRange(from, to))) {
-            text = (title)
-            graphic = new ImageView(LocalRes.DATE_16_PNG)
-          }
-        })
-      }
-
-      val months = (1 to 12).asJava
-
-      flowMonth.setItems(FXCollections.observableArrayList(months))
-
     }
 
-    initMonth()
+    val months = (1 to 12).asJava
+    flowMonth.setItems(FXCollections.observableArrayList(months))
 
 
-    def initYear() = {
-
-      //cell
-      flowYear.setCellFactory { _ =>
-        TextListCell[Int] { year =>
-          if (year == LocalDate.now.getYear) "今年" else s"${year}年"
-        }
+    /**
+     * year
+     */
+    flowYear.setCellFactory { _ =>
+      TextListCell[Int] { year =>
+        if (year == LocalDate.now.getYear) "今年" else s"${year}年"
       }
-
-      //selcet event
-      flowYear.getSelectionModel().selectedItemProperty().addListener { (_, _, year) =>
-        val from = LocalDate.of(year, 1, 1)
-        val to = LocalDate.of(year, 12, 31)
-        val title = s"${year}年"
-
-        logger.debug(s"Range:${from}  ${to}")
-
-
-        TabManager().open(title, {
-          new DataPage(ExplorerData(new ByDateRange(from, to))) {
-            text = (title)
-            graphic = new ImageView(LocalRes.DATE_16_PNG)
-          }
-        })
-      }
-
-      val years = (2000 to LocalDate.now.getYear + 1).reverse.asJava
-
-      flowYear.setItems(FXCollections.observableArrayList(years))
-
     }
 
-    initYear()
+    val years = (2000 to LocalDate.now.getYear + 1).reverse.asJava
+    flowYear.setItems(FXCollections.observableArrayList(years))
+  }
+
+  override protected def eventBinding(): Unit = {
+
+    flowMonth.getSelectionModel().selectedItemProperty().addListener { (_, _, month) =>
+      val from = LocalDate.now.withMonth(month).withDayOfMonth(1)
+      val to = from.plusMonths(1).minusDays(1)
+      val title = s"${month}月"
+
+      logger.debug(s"Range:${from}  ${to}")
+
+      TabManager().open(title, {
+        new DataPage(ExplorerData(new ByDateRange(from, to))) {
+          text = (title)
+          graphic = new ImageView(LocalRes.DATE_16_PNG)
+        }
+      })
+    }
+
+
+    flowYear.getSelectionModel().selectedItemProperty().addListener { (_, _, year) =>
+      val from = LocalDate.of(year, 1, 1)
+      val to = LocalDate.of(year, 12, 31)
+      val title = s"${year}年"
+
+      logger.debug(s"Range:${from}  ${to}")
+
+
+      TabManager().open(title, {
+        new DataPage(ExplorerData(new ByDateRange(from, to))) {
+          text = (title)
+          graphic = new ImageView(LocalRes.DATE_16_PNG)
+        }
+      })
+    }
 
   }
 }

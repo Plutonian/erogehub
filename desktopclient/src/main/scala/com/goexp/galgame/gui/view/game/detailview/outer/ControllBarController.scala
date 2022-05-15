3b4a086dded280d88b3b3319bb414b5e8e3378ad
benbear <benbear0519@gmail.com>
2022-05-15 15:26:32 +0800

    update

--- desktopclient/src/main/scala/com/goexp/galgame/gui/view/game/detailview/outer/ControllBarController.scala
+++ desktopclient/src/main/scala/com/goexp/galgame/gui/view/game/detailview/outer/ControllBarController.scala
@@ -5,6 +5,7 @@ import com.goexp.galgame.gui.view.common.jump.JumpLinkController
 import com.goexp.galgame.gui.view.game.part.{LocationChangeController, StateChangeController}
 import com.goexp.ui.javafx.DefaultController
 import javafx.fxml.FXML
+import scalafx.Includes._
 
 class ControllBarController extends DefaultController {
   @FXML private var webjumpController: JumpLinkController = _
@@ -18,7 +19,10 @@ class ControllBarController extends DefaultController {
 
   private def loadWithoutImage(game: Game) = {
     changeStateController.load(game)
-    changeLocationController.load(game)
+
+    changeLocationController.location <==> game.location
+    changeLocationController.targetGame = game
+
     webjumpController.load(game)
   }
 }
--- desktopclient/src/main/scala/com/goexp/galgame/gui/view/game/part/LocationChangeController.scala
+++ desktopclient/src/main/scala/com/goexp/galgame/gui/view/game/part/LocationChangeController.scala
@@ -4,41 +4,44 @@ import com.goexp.galgame.common.model.game.GameLocation
 import com.goexp.galgame.gui.model.Game
 import com.goexp.galgame.gui.task.game.change.Location
 import com.goexp.ui.javafx.{DefaultController, TaskService}
-import javafx.beans.value.ChangeListener
+import javafx.beans.property.SimpleObjectProperty
 import javafx.collections.FXCollections
 import javafx.fxml.FXML
 import javafx.scene.control.ChoiceBox
+import scalafx.Includes._
 
 import scala.jdk.CollectionConverters._
 
 class LocationChangeController extends DefaultController {
   @FXML private var choiceLocation: ChoiceBox[GameLocation] = _
 
-  private var targetGame: Game = _
+  var targetGame: Game = _
+
+  lazy val location = new SimpleObjectProperty[GameLocation]
+
+
   final private val changeGameLocationService = TaskService(new Location(targetGame))
 
-  private val listener: ChangeListener[GameLocation] = (_, _, newValue) => {
-    if (newValue != null) {
-      logger.debug(s"<Action>Value:${choiceLocation.getValue},New:${newValue}")
-
-      targetGame.location.set(newValue)
-      changeGameLocationService.restart()
-    }
-  }
 
   override protected def initComponent() = {
-    val types = List(GameLocation.REMOTE,
-      //      GameLocation.NETDISK,
-      GameLocation.LOCAL
-    ).asJava
+    val types = Seq(GameLocation.REMOTE, GameLocation.LOCAL).asJava
     choiceLocation.setItems(FXCollections.observableArrayList(types))
   }
 
-  def load(game: Game) = {
 
-    choiceLocation.valueProperty.removeListener(listener)
-    choiceLocation.setValue(game.location.get)
-    targetGame = game
-    choiceLocation.valueProperty.addListener(listener)
+  override protected def dataBinding(): Unit = {
+    choiceLocation.value <==> location
   }
+
+  override protected def eventBinding(): Unit = {
+    location.onChange((_, oldValue, newValue) => {
+      if (oldValue != null && newValue != null) {
+
+        logger.debug(s"<Action>Old:${oldValue},New:${newValue}")
+
+        changeGameLocationService.restart()
+      }
+    })
+  }
+
 }

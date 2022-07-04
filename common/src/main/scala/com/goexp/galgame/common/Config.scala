8f440b1e5577735162cfe2f2d63c7a1e0036bca2
benbear <benbear0519@gmail.com>
2022-07-04 15:35:31 +0800

    update

--- common/src/main/scala/com/goexp/galgame/common/Config.scala
+++ common/src/main/scala/com/goexp/galgame/common/Config.scala
@@ -5,9 +5,9 @@ import java.util.ResourceBundle
 object Config {
   val proxy = true
 
-  private val prop = ResourceBundle.getBundle("config_common")
+  private val prop = ResourceBundle.getBundle("db")
 
   val DB_STRING = prop.getString("mongo")
 
-  val DB_NAME = "galgame"
+  val DB_NAME = prop.getString("database")
 }
